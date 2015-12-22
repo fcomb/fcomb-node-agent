5dc99c3dfdbceeb3d3f2bc0fdd2d1b9847ef0fa8
Timothy Klim <klimtimothy@gmail.com>
2015-12-22 20:51:55 +0300

    chore: pass token through first argument

--- src/Main.hs
+++ src/Main.hs
@@ -1,129 +1,59 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Http
-import Certs
-import Config
-import Globals
-import Docker
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import           Network.HTTP.Client          (defaultManagerSettings)
+import           Network.HTTP.Conduit
+import qualified Data.Conduit                 as C
+import           Data.Conduit.Binary          (sinkFile)
+import           Control.Monad.IO.Class       (liftIO)
+import           Control.Monad.Trans.Resource (runResourceT)
+import           Crypto.Hash
+import           Crypto.PubKey.RSA
+import           Data.PEM
+import           Data.X509
+import           Data.X509.PKCS10
+import qualified Network.Wreq as WQ
+-- import Control.Lens
+import Data.Aeson (toJSON)
+import Data.Aeson.Lens (key, nth)
+import Data.Aeson.Types
 import System.Environment
-import System.Exit
-import System.Directory
-import System.FilePath
-import System.Posix.Files
-import System.Posix.Process
-import Control.Concurrent
+
+rsaKeySize = 256
+
+publicExponent = 0x10001
+
+newtype JoinRequest =
+  JoinRequest { certificationRequest :: String }
+  deriving (Show, Eq)
+
+instance ToJSON JoinRequest where
+  toJSON (JoinRequest req) =
+    object ["certificationRequest" .= req]
 
 main :: IO ()
 main = do
-    args <- getArgs
+  (pubKey, privKey) <- generate rsaKeySize publicExponent
 
-    token <- case args of
-        a1:as | not (null a1) -> do
-            putStrLn $ "Got agent token: " ++ a1
-            return (a1)
-        _ -> do
-            putStrLn "Token is empty! Provide token as the first argument to the agent"
-            exitFailure
+  let subjectAttrs = makeX520Attributes [(X520CommonName, "node.fcomb.io"), (X520OrganizationName, "fcomb")]
+  let extAttrs = PKCS9Attributes [PKCS9Attribute $ ExtBasicConstraints False Nothing, PKCS9Attribute $ ExtKeyUsage [KeyUsage_digitalSignature,KeyUsage_nonRepudiation,KeyUsage_keyEncipherment]]
+  Right req <- generateCSR subjectAttrs extAttrs (KeyPairRSA pubKey privKey) SHA512
+  BC.putStrLn . pemWriteBS . toPEM $ req -- export in PEM format
+  putStrLn . show $ verify (csrToSigned req) $ PubKeyRSA pubKey -- sign CSR before verify
 
-    let dockerBinPath = combine dockerDir dockerBinaryName
-        --dockerNewBinPath = combine dockerDir dockerNewBinaryName
-        --dockerNewBinSigPath = combine dockerDir dockerNewBinarySigName
-        --configFilePath = combine fcombHome configFileName
-        keyFilePath = combine fcombHome keyFileName
-        certFilePath = combine fcombHome certFileName
-        caFilePath = combine fcombHome cAFileName
+  let pem = BC.unpack . pemWriteBS . toPEM $ req
+  let joinReq = JoinRequest pem
+  joinToken <- head <$> getArgs
+  let url = "http://api/v1/nodes/join?access_token=" ++ joinToken
+  r <- WQ.post url (toJSON joinReq)
+  -- res <- (r ^? responseBody . key "json" . nth 0)
+  putStrLn . show $ r
 
-    -- create directories with parents and default permissions of 755
-    createDirectoryIfMissing True fcombHome
-    createDirectoryIfMissing True dockerDir
-
-    -- removing old certificates
-    fileExist keyFilePath >>= \exists -> if exists
-        then removeFile keyFilePath
-        else return ()
-
-    fileExist certFilePath >>= \exists -> if exists
-        then removeFile certFilePath
-        else return ()
-
-    fileExist caFilePath >>= \exists -> if exists
-        then removeFile caFilePath
-        else return ()
-
-
-    putStrLn $ "Running node-agent: version " ++ agentVersion
-    --createPidFile fcombPidFile
-
---    putStrLn "Checking if config file exists"
---    fileExist configFilePath >>= \exists -> if exists
---        then
---            return ()
---        else do
---            putStrLn "Writing a config file with default settings"
---            saveConf configFilePath defaultConf
---
---    putStrLn "Loading Configuration file"
---    conf <- loadConf configFilePath
-
-    cert <- createCerts keyFilePath
-    putStrLn $ "Generated certificate: \n" ++ cert
-    let regUrl = defaultFcombHost ++ regEndpoint
-    putStrLn $ "Registering with fcomb: " ++ regUrl
-    register regUrl token cert caFilePath certFilePath
-
-    -- download docker binary if missing
-    fileExist dockerBinPath >>= \exists -> if exists
-        then
-            return ()
-        else do
-            putStrLn "Downloading docker binary..."
-            download dockerBinaryURL dockerBinPath
-            --removeLink dockerSymbolicLink
-            --createSymbolicLink path dockerSymbolicLink
-
-    putStrLn "Initializing docker daemon"
-    --dockerHandle <- startDocker dockerBinPath keyFilePath certFilePath caFilePath defaultDockerHost
-    putStrLn "Docker daemon has been started"
-    --putStrLn "Verifying the registration with Fcomb"
-    --verifyRegistration regUrl
-
-    --putStrLn "Docker server started. Entering maintenance loop"
-    --maintenanceLoop
-
-    putStrLn "Over"
-
-
---maintenanceLoop = do
---    threadDelay heartBeatInterval
---    updateDocker dockerBinPath dockerNewBinPath dockerNewBinSigPath keyFilePath certFilePath caFilePath handle
---
---    if dockerProcess == nil
---        threadDelay heartBeatInterval * 1000 * 1000
---        if dockerProcess == nil ScheduleToTerminateDocker == false
---            putStrLn "Respawning docker daemon"
---            startDocker
---
---    maintenanceLoop
-
-
-checkPidFile :: FilePath -> IO ()
-checkPidFile pidFile =
-    fileExist pidFile >>= \exists -> if exists
-        then do
-            pid <- readFile pidFile
-            let procFile = combine "/proc" pid
-            fileExist procFile >>= \exists -> if exists
-                then
-                    putStrLn $ "Found pid file, make sure that fcomb-agent is not running or remove " ++ pidFile
-                else do
-                    return ()
-        else
-            return ()
-
-
-createPidFile :: FilePath -> IO ()
-createPidFile pidFile =
-    getProcessID >>= \pid -> do
-        checkPidFile pidFile
-        writeFile pidFile (show pid)
-        putStrLn $ "Created pid file( " ++ pidFile ++ "): " ++ (show pid)
+  -- manager <- newManager defaultManagerSettings
+  -- request <- liftIO $ parseUrl "http://coreos:2375/containers/ubuntu_tty/archive?path=/"
+  -- runResourceT $ do
+  --   response <- http request manager
+  --   responseBody response C.$$+- sinkFile "/tmp/docker.dump"
